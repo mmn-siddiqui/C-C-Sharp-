c575c9ad15455fe8c2508fe858d02e9a340f847a
Muhammad Mubashir Noor <64870452+mmn-siddiqui@users.noreply.github.com>
2020-07-21 12:15:22 +0500

    Add files via upload

---
+++ ShoppingCart/ShoppingCart/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace ShoppingCart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.buttonAddToCart = new System.Windows.Forms.Button();
+            this.buttoncheckout = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Brush Script MT", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(187, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shopping Cart";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(29, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(29, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select Item";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(29, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantity";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblAmount.Location = new System.Drawing.Point(34, 422);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(71, 20);
+            this.lblAmount.TabIndex = 4;
+            this.lblAmount.Text = "Amount";
+            // 
+            // buttonAddToCart
+            // 
+            this.buttonAddToCart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.buttonAddToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddToCart.Location = new System.Drawing.Point(270, 318);
+            this.buttonAddToCart.Name = "buttonAddToCart";
+            this.buttonAddToCart.Size = new System.Drawing.Size(107, 28);
+            this.buttonAddToCart.TabIndex = 5;
+            this.buttonAddToCart.Text = "Add to cart";
+            this.buttonAddToCart.UseVisualStyleBackColor = false;
+            this.buttonAddToCart.Click += new System.EventHandler(this.buttonAddToCart_Click);
+            // 
+            // buttoncheckout
+            // 
+            this.buttoncheckout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.buttoncheckout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttoncheckout.Location = new System.Drawing.Point(270, 390);
+            this.buttoncheckout.Name = "buttoncheckout";
+            this.buttoncheckout.Size = new System.Drawing.Size(153, 58);
+            this.buttoncheckout.TabIndex = 6;
+            this.buttoncheckout.Text = "Checkout";
+            this.buttoncheckout.UseVisualStyleBackColor = false;
+            this.buttoncheckout.Click += new System.EventHandler(this.buttoncheckout_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "TShirt",
+            "Jeans",
+            "Trouser",
+            "Footwear",
+            "Jacket"});
+            this.comboBox1.Location = new System.Drawing.Point(178, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 28);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(179, 146);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(189, 64);
+            this.listBox1.TabIndex = 8;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(178, 260);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(190, 26);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(438, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 332);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "adidastrouser.jpg");
+            this.imageList1.Images.SetKeyName(1, "adidasshoes.jpg");
+            this.imageList1.Images.SetKeyName(2, "levis.jpg");
+            this.imageList1.Images.SetKeyName(3, "zaramanjacket.jpg");
+            this.imageList1.Images.SetKeyName(4, "nikeshoes.jpg");
+            this.imageList1.Images.SetKeyName(5, "outfittersjacket.jpg");
+            this.imageList1.Images.SetKeyName(6, "outfittersjeans.jpg");
+            this.imageList1.Images.SetKeyName(7, "pumatrouser.jpg");
+            this.imageList1.Images.SetKeyName(8, "superman.jpg");
+            this.imageList1.Images.SetKeyName(9, "supreme.jpg");
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(134, 427);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(34, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Click += new System.EventHandler(this.label5_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 422);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(161, 318);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 28);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Reset Cart";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(749, 515);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttoncheckout);
+            this.Controls.Add(this.buttonAddToCart);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Button buttonAddToCart;
+        private System.Windows.Forms.Button buttoncheckout;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ShoppingCart/ShoppingCart/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingCart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int amount = 0;
+        int quantity; 
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string s = comboBox1.SelectedItem.ToString();
+            pictureBox1.Image = new Bitmap(s);
+            listBox1.Items.Clear();
+
+            if (comboBox1.Text == @"D:\Mubashir\ShoppingCart\imgs\3.jpg")
+            {
+                listBox1.Items.Add("Supreme : Rs 1200");
+            }
+            else if (comboBox1.Text == @"D:\Mubashir\ShoppingCart\imgs\1.jpg")
+            { 
+                listBox1.Items.Add("Adidas : Rs 7800");
+            }
+            else if (comboBox1.Text == @"D:\Mubashir\ShoppingCart\imgs\2.jpg")
+            {
+                listBox1.Items.Add("Puma : Rs 1200");
+               
+            }
+            else if (comboBox1.Text == @"D:\Mubashir\ShoppingCart\imgs\4.jpg")
+            {
+                listBox1.Items.Add("Zara Man : Rs 12000");
+               
+            }
+        }
+
+        private void buttonAddToCart_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem.ToString() == "Supreme : Rs 1200")
+            {
+
+                quantity = Convert.ToInt32(numericUpDown1.Value);
+                amount += 1200 * quantity;
+                textBox1.Text = Convert.ToString(amount);
+
+            }
+            /* else if(listBox1.SelectedItem.ToString() == "Superman : Rs 1000")
+             {
+                 quantity = Convert.ToInt32(numericUpDown1.Value);
+                 amount += 1000 * quantity;
+                 lblAmount.Text = Convert.ToString(amount);
+             }
+             else if (listBox1.SelectedItem.ToString() == "Levis : Rs 5000")
+             {
+                 quantity = Convert.ToInt32(numericUpDown1.Value);
+                 amount += 5000 * quantity;
+                 lblAmount.Text = Convert.ToString(amount);
+             }*/
+            /* else if (listBox1.SelectedItem.ToString() == "Outfitters: Rs 4000")
+             {
+                 quantity = Convert.ToInt32(numericUpDown1.Value);
+                 amount += 4000 * quantity;
+                 lblAmount.Text = Convert.ToString(amount);
+             }
+             else if (listBox1.SelectedItem.ToString() == "Nike : Rs 8000")
+             {
+                 quantity = Convert.ToInt32(numericUpDown1.Value);
+                 amount += 8000 * quantity;
+                 lblAmount.Text = Convert.ToString(amount);
+             }*/
+            else if (listBox1.SelectedItem.ToString() == "Adidas : Rs 7800")
+            {
+                quantity = Convert.ToInt32(numericUpDown1.Value);
+                amount += 7800 * quantity;
+                textBox1.Text = Convert.ToString(amount);
+            }
+            else if (listBox1.SelectedItem.ToString() == "Puma : Rs 1200")
+            {
+                
+                 quantity = Convert.ToInt32(numericUpDown1.Value);
+                 amount += 1200 * quantity;
+                 textBox1.Text = Convert.ToString(amount);
+            }
+           /* else if (listBox1.SelectedItem.ToString() == "Adidas : Rs 1400")
+            {
+                quantity = Convert.ToInt32(numericUpDown1.Value);
+                amount += 1400 * quantity;
+                label5.Text = Convert.ToString(amount);
+            }*/
+            else if (listBox1.SelectedItem.ToString() == "Zara Man : Rs 12000")
+            {
+               
+                quantity = Convert.ToInt32(numericUpDown1.Value);
+                amount += 12000 * quantity;
+                textBox1.Text = Convert.ToString(amount);
+            }
+           /* else if (listBox1.SelectedItem.ToString() == "Outfitters : Rs 9000")
+            {
+                quantity = Convert.ToInt32(numericUpDown1.Value);
+                amount += 9000 * quantity;
+                lblAmount.Text = Convert.ToString(amount);
+            }*/
+        }
+
+        private void buttoncheckout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your order has been placed");
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Clear();
+            string[] imgs = Directory.GetFiles(@"D:\Mubashir\ShoppingCart\imgs");
+            foreach (string file in imgs)
+            {
+                comboBox1.Text = "Select Data";
+                comboBox1.Items.Add(file);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            amount = 0;
+        }
+    }
+
+}
